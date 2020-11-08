7273896d256c957bc5128fabf0273e14dab06bf2
zmproni <zmproni@gmail.com>
2020-11-08 23:24:50 +0800

    First commit

---
+++ backend/src/main/java/com/intians/hack2hire/Hack2hireApplication.java
@@ -0,0 +1,13 @@
+package com.intians.hack2hire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hack2hireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hack2hireApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/intians/hack2hire/Hack2hireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intians.hack2hire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hack2hireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
